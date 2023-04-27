08d40e57a73727fc7fffccdce051dd09d0b06584
rabBit64 <gojh47@gmail.com>
2023-04-27 22:05:15 +0900

    feat: django & react 연동

--- crud/shoppings/views.py
+++ crud/shoppings/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import WishList
+def init(request):
+    return redirect('shoppings:index')
+
 def index(request):
     wishlists = WishList.objects.all()
     context = {
